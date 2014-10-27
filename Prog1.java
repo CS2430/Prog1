6a6680d82cdec5303307c37dfa5658cc803473e7
narisha-reddy <narisha@tds.net>
2014-10-26 19:37:12 -0500

    Create Prog1.java

---
+++ Prog1.java
@@ -0,0 +1,19 @@
+
+/**
+ * Runs the main for Program 1 -- 2014
+ *
+ * @author narishareddy
+ */
+public class Prog1
+{
+
+   /**
+    * Runs TableTennisPlayerDatabase.
+    *
+    * @param args is unused
+    */
+   public static void main(String args[])
+   {
+      new TableTennisPlayerDatabase().run();
+   }
+}
